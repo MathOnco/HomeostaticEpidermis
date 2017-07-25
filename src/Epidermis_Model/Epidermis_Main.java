998664f689c7e27436d39991c77661392e7e212a
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2017-07-25 09:52:05 -0400

    Ready for Sharing, (iMac)

--- src/Epidermis_Model/Epidermis_Main.java
+++ src/Epidermis_Model/Epidermis_Main.java
@@ -29,15 +29,15 @@ class EpidermisConst{
 
     static final int VisUpdate = 7; // Timestep interval to update Division and Death, etc.
 
-    static final boolean GuiOn = false; // use for visualization, set to false for jar file / multiple runs
+    static final boolean GuiOn = true; // use for visualization, set to false for jar file / multiple runs
     static final boolean JarFile = true; // Set to true if running from command line as jar file!!!!!!!!
-    static final boolean RecordParents = true; // use when you want parents information
-    static final boolean RecordLineages = true; // use when you want
-    static final boolean RecordPopSizes = true; // Use to record clone population sizes
+    static final boolean RecordParents = false; // use when you want parents information
+    static final boolean RecordLineages = false; // use when you want
+    static final boolean RecordPopSizes = false; // Use to record clone population sizes
     static final boolean get_r_lambda = true; // use when you want the r_lambda value for the visualization
-    static final boolean writeValues = true; // use this when you want the data to be saved!
-    static final boolean sliceOnly = true; // use this when you want only a slice of the 3D model to be output!!!!!!!!!!!!!!
-    static final boolean SliceAndFull = true; // use this when you want a slice out of the 3D model and the full data of the modeled cells!!!!
+    static final boolean writeValues = false; // use this when you want the data to be saved!
+    static final boolean sliceOnly = false; // use this when you want only a slice of the 3D model to be output!!!!!!!!!!!!!!
+    static final boolean SliceAndFull = false; // use this when you want a slice out of the 3D model and the full data of the modeled cells!!!!
     static final boolean GetImageData = false; // Use for 3D data for visualization
 }
 
@@ -85,17 +85,17 @@ public class Epidermis_Main {
         Sets up Data Files if on cluster or if ran locally
          */
         if(EpidermisConst.JarFile){
-            ParentFile = args[0];
-            PopSizes = args[1];
-            MutationFile = args[2];
-            r_lambda_file = args[3];
-            EpidermisConst.xSize = Integer.parseInt(args[4]);
-            EpidermisConst.zSize = Integer.parseInt(args[4]);
-            int Time = Integer.parseInt(args[5]);
+//            ParentFile = args[0];
+//            PopSizes = args[1];
+//            MutationFile = args[2];
+//            r_lambda_file = args[3];
+            EpidermisConst.xSize = Integer.parseInt(args[0]);
+            EpidermisConst.zSize = Integer.parseInt(args[0]);
+            int Time = Integer.parseInt(args[1]);
             EpidermisConst.years = Time;
             EpidermisConst.ModelTime = Time * 365 + 10;
             EpidermisConst.RecordTime = Time * 365;
-            PositionFile = args[6];
+//            PositionFile = args[6];
         }
         if(EpidermisConst.GuiOn == false && EpidermisConst.GetImageData == false){
             System.out.println("xSize and zSize: " + EpidermisConst.xSize);
